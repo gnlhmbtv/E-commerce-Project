ee57feaf98c7b0491c8db6eaba90cd37939deeee
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-03-12 18:45:32 +0400

    feat: added sorting

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,9 +34,10 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductReturnDto>>> GetProducts(
+            string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
 
             var products = await _productsRepository.ListAsync(spec);
             

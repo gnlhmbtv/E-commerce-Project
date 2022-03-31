95761ed3b0ddc8c966b525c14e2d0801f0fbe32a
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-03-31 23:22:08 +0400

    feat: added paging component

---
+++ client/src/app/shared/components/paging/paging.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-paging',
+  templateUrl: './paging.component.html',
+  styleUrls: ['./paging.component.css']
+})
+export class PagingComponent implements OnInit {
+  @Input() totalCount: number;
+  @Input() pageSize: number;
+  @Output() pageChanged = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onPagerChanged(event: any){
+    this.pageChanged.emit(event.page);
+  }
+
+}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {PaginationModule} from 'ngx-bootstrap/pagination';
+import {PagerComponent, PaginationModule} from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagingComponent } from './components/paging/paging.component';
 
 @NgModule({
   declarations: [
     OrderTotalsComponent,
-    PagingHeaderComponent
+    PagingHeaderComponent,
+    PagingComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     PaginationModule.forRoot()
   ],
-  exports: [OrderTotalsComponent, ReactiveFormsModule, PaginationModule, PagingHeaderComponent]
+  exports: [OrderTotalsComponent, ReactiveFormsModule, PaginationModule, PagingHeaderComponent, PagingComponent]
 })
 export class SharedModule { }

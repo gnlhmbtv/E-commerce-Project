3027e2372e58b74ec1eb35ec2cdf9c1afb0b73ce
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-03-31 22:58:46 +0400

    feat: added pagination header component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {PaginationModule} from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 
 @NgModule({
   declarations: [
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    PagingHeaderComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     PaginationModule.forRoot()
   ],
-  exports: [OrderTotalsComponent, ReactiveFormsModule, PaginationModule]
+  exports: [OrderTotalsComponent, ReactiveFormsModule, PaginationModule, PagingHeaderComponent]
 })
 export class SharedModule { }

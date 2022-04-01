5e5a37284b7ec1e186050c1f6082d9e89ac6a803
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-04-02 00:36:58 +0400

    feat: created order aggregate

---
+++ Core/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,21 @@
+namespace Core.Entities.OrderAggregate
+{
+    public class ProductItemOrdered
+    {
+        public ProductItemOrdered()
+        {
+
+        }
+        
+        public ProductItemOrdered(int productItemId, string productName, string photoUrl) 
+        {
+            ProductItemId = productItemId;
+            ProductName = productName;
+            PhotoUrl = photoUrl;
+               
+        }
+                public int ProductItemId { get; set; }
+        public string ProductName { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}

58f1d156ddb99944bd6ece590508dde1ed476fa4
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-04-01 10:52:48 +0400

    feat: created auth guard

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,34 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  returnUrl: string;
 
-  constructor(private accountService: AccountService, private router: Router) { }
+  constructor(
+    private accountService: AccountService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/shop';
     this.createLoginForm();
   }
 
-  createLoginForm(){
+  createLoginForm() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
-      password: new FormControl('', Validators.required)
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+      ]),
+      password: new FormControl('', Validators.required),
     });
   }
 
-  onSubmit(){
-    this.accountService.login(this.loginForm.value).subscribe(() => {
-      this.router.navigateByUrl('/shop');
-    }, error => {
-      console.log(error);
-    });
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe(
+      () => {
+        this.router.navigateByUrl(this.returnUrl);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }

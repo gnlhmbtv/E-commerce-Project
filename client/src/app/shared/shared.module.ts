61f1fa4b16e5a2157b1df285489f12bf5dd026b2
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-04-03 13:02:24 +0400

    feat: basket summary changed to shared

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,23 +2,41 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {PagerComponent, PaginationModule} from 'ngx-bootstrap/pagination';
+import { PagerComponent, PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagingComponent } from './components/paging/paging.component';
 import { TextInputComponent } from './components/text-input/text-input.component';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { StepperComponent } from './components/stepper/stepper.component';
+import { BasketSummaryComponent } from './components/basket-summary/basket-summary.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     OrderTotalsComponent,
     PagingHeaderComponent,
     PagingComponent,
-    TextInputComponent
+    TextInputComponent,
+    StepperComponent,
+    BasketSummaryComponent,
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CdkStepperModule,
+    RouterModule
   ],
-  exports: [OrderTotalsComponent, ReactiveFormsModule, PaginationModule, TextInputComponent, PagingHeaderComponent, PagingComponent]
+  exports: [
+    OrderTotalsComponent,
+    ReactiveFormsModule,
+    PaginationModule,
+    TextInputComponent,
+    PagingHeaderComponent,
+    PagingComponent,
+    CdkStepperModule,
+    StepperComponent,
+    BasketSummaryComponent
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
